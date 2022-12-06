041454d73701903ab6a7c0b1a77c11f830e2a676
tdoff <temirrrlan@gmail.com>
2022-12-06 15:11:05 +0600

    Initial commit

---
+++ src/test/java/com/example/springcarnage/SpringCarnageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcarnage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCarnageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
